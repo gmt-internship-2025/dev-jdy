f1ec987556eab64f3335fb0f09b6407f2a4b4658
JDY323 <da5457@naver.com>
2025-08-13 13:10:10 +0900

    feat: /button<num> 라우트 추가 #13

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-# app.py
+# app1.py
+
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -6,11 +7,11 @@ gaze_coords = {"x": 0, "y": 0}
 
 @app.route('/')
 def index():
-    return render_template("index1.html")
+    return render_template("index2.html")
 
-@app.route('/next')
-def next_page():
-    return "<h1> 이동 완료! 다른 화면입니다.</h1>"
+@app.route('/button<int:num>')
+def button_page(num):
+    return f"<h1>Button {num} 페이지 입니다.</h1>"
 
 @app.route('/api/gaze', methods=['POST'])
 def update_gaze():
